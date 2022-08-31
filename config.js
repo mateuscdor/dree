dc6c31f106f119e94948745d22a6a8bc4d72d2de
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-09-01 04:54:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -16,6 +16,46 @@ module.exports = {
         API_KEY: process.env.HEROKU_API_KEY || '',
         APP_NAME: process.env.HEROKU_APP_NAME || ''
     },
+    profile: {
+    ownerName: "inrl", 
+    ownerNumb: "", 
+    botName: "inrl-bot-md", 
+  },
+    reply: {
+    notFound: ""This Command not created. it was creating",
+    success: "Done ✓",
+    admin: "This Feature Is Only For Admin",
+    botAdmin: "Bot Must Be Admin First", 
+    owner: "This Feature Is Only For Owner",
+    group: "Feature Used Only For Groups!", 
+    private: "Features Used Only For Private Chat!", 
+    bot: "This Feature Is Only For Bot", 
+    wait: "In process..",
+    linkm: "", 
+    error: "Error!!", 
+    endLimit: "Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours",
+    ban: "You have been banned by the owner, if you want to be unbanned, chat owner", 
+    nsfw: "The nsfw feature has not been activated, please contact the admin to activate", 
+    banChat: "The bot was banned in this group, please contact the owner to unban", 
+  },
+    setting: {
+    blockchat: [], // Your block chat Jids
+  },
+  auto: {
+    chat: {
+      group: false, // Chat Bot In Group | u can set true or false
+      inbox: false, // chat bot in inbox | u can set true or false
+    },
+    reply: {
+      sticker: false, // Boolean | ===== It not created now ======
+      audio: false, // Boolean | ===== It not created now ======
+    },
+    presence: {
+      is: false, // U Can on or off this () | u can set true or false
+      value: "recording", // It has two types | u can set 'recoding' or 'typing'
+    },
+    read: false, // Boolean | ===== It not created now ======
+  },
     DATABASE_URL: DATABASE_URL,
     DATABASE: DATABASE_URL === './bot.db' ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: DEBUG }) : new Sequelize(DATABASE_URL, { dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }, logging: DEBUG }),
     NO_ONLINE: process.env.NO_ONLINE === undefined ? true : convertToBool(process.env.NO_ONLINE),
